ac05b6bcd76454f4b8477cba862f60ba209945de
Buenvenida <buenvenidake@students.nu-lipa.edu.ph>
2025-05-15 11:07:20 +0800

    new update

---
+++ ajax/check_username.php
@@ -0,0 +1,20 @@
+<?php
+ 
+require_once('../classes/database.php');
+ 
+$con = new database();
+ 
+if (isset($_POST['username'])){
+    $username = $_POST['username'];
+ 
+    //Check if the username exists
+    if ($con->isUsernameExists($username)){
+        echo json_encode(['exists'=> true]);
+    }else{
+        echo json_encode(['exists'=> false]);
+    }
+ 
+}else{
+    echo json_encode(['error'=> 'Invalid request']);
+    }
+ 
