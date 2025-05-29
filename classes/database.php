3c9cd7c7765f0b841541afa536dc9efebd6ac9f8
Buenvenida <buenvenidake@students.nu-lipa.edu.ph>
2025-05-29 09:43:42 +0800

    Updated

--- classes/database.php
+++ classes/database.php
@@ -8,7 +8,6 @@ class database{
             'mysql:host=localhost; dbname=dbs_app',
             username: 'root',
             password: '');
-   
         }
  
         function signupUser($firstname, $lastname, $username, $email, $password){
@@ -36,20 +35,36 @@ class database{
  
             $stmt = $con->prepare("SELECT COUNT(*) FROM Admin WHERE admin_username = ?");
             $stmt->execute([$username]);
-            
+ 
             $count = $stmt->fetchColumn();
-            
-            return $count > 0; //Returns true if username exists, false otherwise
+            return $count > 0;
            
         }
+ 
         function isEmailExists($email) {
             $con = $this->opencon();
  
             $stmt = $con->prepare("SELECT COUNT(*) FROM Admin WHERE admin_email = ?");
             $stmt->execute([$email]);
-
+ 
             $count = $stmt->fetchColumn();
-            
             return $count > 0;
-}
+           
+        }
+ 
+       function loginUser($username, $password) {
+            $con = $this->opencon();
+ 
+            $stmt = $con->prepare("SELECT * FROM Admin WHERE admin_username = ?");
+            $stmt->execute([$username]);
+ 
+            $user = $stmt->fetch (PDO::FETCH_ASSOC);
+           
+            if ($user && password_verify($password, $user['admin_password'])) {
+           
+            return $user;
+            }else{
+                return false;
+        }
+    }
 }
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+    session_start();
+    if (!isset($_SESSION["admin_id"])){
+      header('Location: login.php');
+        exit();
+    } 
     require_once('classes/database.php');
     $con = new database();
 
